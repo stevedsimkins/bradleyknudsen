673b6053bfac59b62fa613165fc091de17e7437f
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-10-19 21:23:47 -0400

    added contribution

--- src/components/bradley/Footer.jsx
+++ src/components/bradley/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
   return (
     <FooterContainer>
       <FooterVector src={bottomVector} alt="graphic" />
+      <p>Designed by stevedsimkins</p>
     </FooterContainer>
   )
 };
@@ -14,11 +15,20 @@ const FooterContainer = styled.div`
   min-height: 50vh;
   width: 100%;
   position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  p {
+    text-align: center;
+    bottom: 10%;
+    z-index: 3;
+  }
 `;
 
 const FooterVector = styled.img`
   position: absolute;
   bottom: -20%;
+  width: 100%;
 `;
 
 const FooterText = styled.p`
