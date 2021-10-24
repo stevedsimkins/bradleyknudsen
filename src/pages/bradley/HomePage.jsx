d6e815c62eca51f381d06bbaf568fdd284b94174
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-10-23 21:02:41 -0400

    added more music, finsihed music player for now

--- src/pages/bradley/HomePage.jsx
+++ src/pages/bradley/HomePage.jsx
@@ -7,12 +7,23 @@ import Intro from "../../components/bradley/Intro";
 import Work from "../../components/bradley/Work";
 import Contact from "../../components/bradley/Contact";
 import Footer from "../../components/bradley/Footer";
-import track1 from "../../audio/track1.wav";
+import cover from "../../assets/coverphoto1.png";
+import bellsFromTheDepths from "../../audio/bells-from-the-depths.m4a";
+import fracturedFaith from "../../audio/fractured-faith.m4a";
+import intoToPsychocartography from "../../audio/intro-to-psychocartography.m4a";
+import rainOnALake from "../../audio/rain-on-a-lake.m4a";
+import sleepyHollow from "../../audio/sleepy-hollow.m4a";
+import theTransfiguration from "../../audio/the-transfiguration.m4a";
 
 const HomePage = () => {
 
   const [audioList, setAudioList] = useState([
-    { musicSrc: track1 }
+    { musicSrc: bellsFromTheDepths, name: "Bells from the Depths", cover: cover, singer: "Bradley Knudsen" },
+    { musicSrc: intoToPsychocartography, name: "Intro to Psychocartography", cover: cover, singer: "Bradley Knudsen" },
+    { musicSrc: fracturedFaith, name: "Fractured Faith", cover: cover, singer: "Bradley Knudsen" },
+    { musicSrc: rainOnALake, name: "Rain on a Lake", cover: cover, singer: "Bradley Knudsen" },
+    { musicSrc: sleepyHollow, name: "Sleepy Hollow", cover: cover, singer: "Bradley Knudsen" },
+    { musicSrc: theTransfiguration, name: "The Transfiguration", cover: cover, singer: "Bradley Knudsen" },
   ]);
 
 
@@ -23,6 +34,7 @@ const HomePage = () => {
         theme={"light"}
         showDownload={false}
         audioLists={audioList}
+        remove={false}
       />
       <Home />
       <Intro />
