fdda0cb4f21e6ae1f1ea94625066eb9dcd52b16b
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-11-02 21:29:46 -0400

    fixed mobile svgs

--- src/components/bradley/Home.jsx
+++ src/components/bradley/Home.jsx
@@ -52,64 +52,24 @@ const Logo = styled.h1`
 `;
 
 const TopVector = styled.img`
-  width: 100%;
-  height: 25%;
   position: absolute;
   top: 0;
   transition: all 0.5s ease-in-out;
-  @media(min-width: 700px){
-    height: 30%;
-  }
-  @media(min-width: 750px){
-    top: -10%; 
-  }
+ }
 `;
 
 const LeftVector = styled.img`
-  width: 100%;
+  left: 0;
+  bottom: -10%;
   position: absolute;
-  left: -60%;
-  bottom: 0;
   transition: all 0.5s ease-in-out;
-  @media(min-width: 500px){
-    bottom: -30%;
-    left: -40%;
-  }
-  @media(min-width: 750px){
-    bottom: -40%;
-    left: -50%;
-  }
-  @media(min-width: 900px){
-    width: 90%;
-  }
-  @media(min-width: 1100px){
-    width: 80%;
-    bottom: -50%;
-  }
 `;
 
 const RightVector = styled.img`
-  width: 350px;
   position: absolute;
-  bottom: -9%;
-  right: -60%;
+  right: 0;
+  bottom: -10%;
   transition: all 0.5s ease-in-out;
-  @media(min-width: 500px){
-    right: 0;
-    width: 50%;
-    bottom: -20%;
-  }
-  @media(min-width: 750px){
-    width: 45%;
-    bottom: -30%;
-  }
-  @media(min-width: 900px){
-    bottom: -40%;
-  }
-  @media(min-width: 1100px){
-    width: 40%;
-    right: 0;
-  }
 `
 
 export default Home;
